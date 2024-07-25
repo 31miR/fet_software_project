4e12c30a6f06c45d396a149bac67ded5bfc45e6f
31miR <76799355+31miR@users.noreply.github.com.>
2024-07-25 15:42:18 +0200

    add more test stuff for myDB

--- src/Application.java
+++ src/Application.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 import javax.persistence.*;
 
 public class Application {
@@ -6,10 +8,18 @@ public class Application {
 		// TODO Auto-generated method stub
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("myPersistenceUnit");
 	     EntityManager em = emf.createEntityManager();
-
+	     
+	     List<Product> results = em.createQuery("SELECT a FROM Product a", Product.class).getResultList();
+	     
+	     System.out.println(results.size());
+	     for (Product i : results) {
+	    	 System.out.println("" + i.getId() + ", " + i.getName() + ", " + i.getPrice());
+	     }
+	     
+	     long nextId = results.size() + 1L;
 	     // Create and persist a new entity
 	     em.getTransaction().begin();
-	     Product product = new Product(1L, "Laptop", 2.5);
+	     Product product = new Product(nextId, "Laptop", 2.5);
 	     em.persist(product);
 	     em.getTransaction().commit();
 
--- src/Product.java
+++ src/Product.java
@@ -8,9 +8,14 @@ public class Product {
 	private String name;
 	private double price;
 	
+	public Product() {}
+	
 	public Product(Long i, String n, double p) {
 		id = i;
 		name = n;
 		price = p;
 	}
+	public Long getId() {return id;}
+	public String getName() {return name;}
+	public double getPrice() {return price;}
 }
