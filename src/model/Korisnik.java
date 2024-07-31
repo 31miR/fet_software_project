8e8132a9ce018fa41b7d9004e7fb3c557e4db090
31miR <76799355+31miR@users.noreply.github.com.>
2024-07-31 22:20:45 +0200

    add POJO's for Administrator, Korisnik and Organizator entities

---
+++ src/model/Korisnik.java
@@ -0,0 +1,72 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+public class Korisnik {
+	@Id
+	private String username;
+	private String password;
+	private String email;
+	private String name;
+	private String lastName;
+	private String address;
+	private String phone;
+	private double walletBalance;
+	private double discountRate;
+	//TODO: dodati relacijsko mapiranje sa Karta entity
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public double getWalletBalance() {
+		return walletBalance;
+	}
+	public void setWalletBalance(double walletBalance) {
+		this.walletBalance = walletBalance;
+	}
+	public double getDiscountRate() {
+		return discountRate;
+	}
+	public void setDiscountRate(double discountRate) {
+		this.discountRate = discountRate;
+	}
+}
---
+++ src/model/Organizator.java
@@ -0,0 +1,57 @@
+package model;
+
+import javax.persistence.Id;
+
+public class Organizator {
+	@Id
+	private String username;
+	private String password;
+	private String email;
+	private String name;
+	private String lastName;
+	private String address;
+	private String phone;
+	//TODO: dodatna relacijska mapiranja sa entitijima kao sto su dogadjaji
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+}
