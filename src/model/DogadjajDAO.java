ec6fcd89b3653bb7db5f34cc8cc7ebe98bd9e134
31miR <76799355+31miR@users.noreply.github.com.>
2024-08-13 14:54:16 +0200

    create DogadjajDAO.java with getFiltered function

---
+++ src/model/DogadjajDAO.java
@@ -0,0 +1,28 @@
+package model;
+
+import misc.EntityManagerFactoryHolder;
+
+import java.util.List;
+
+import javax.persistence.*;
+
+
+public class DogadjajDAO {
+	private EntityManagerFactory emf = EntityManagerFactoryHolder.getEntityManagerFactory();
+	List<Dogadjaj> getFiltered(List<String> filters, String sortBy, boolean ascending, int start, int ammount) {
+		String where_part = "";
+		for(int i = 0; i < filters.size(); i += 2) {
+			if (i != 0) {
+				where_part += ",";
+			}
+			where_part += "a."+filters.get(i) + "=" + filters.get(i+1);
+		}
+		String sort_part = sortBy + " " + (ascending ? "ASC" : "DESC");
+		String limit_part = String.valueOf(start) + ", " + String.valueOf(ammount);
+		EntityManager em = emf.createEntityManager();
+		List<Dogadjaj> ret = em.createQuery("Select a FROM Dogadjaj a WHERE " + where_part + " ORDER BY "
+				+ sort_part + " LIMIT " + limit_part, Dogadjaj.class).getResultList();
+		em.close();
+		return ret;
+	}
+}
