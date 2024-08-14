c886d5af1e3e15d89da4b9fe44023a86627504cc
31miR <76799355+31miR@users.noreply.github.com.>
2024-08-14 23:26:51 +0200

    format code in KorisnikDAO.java (delete unnecessary newlines)

--- src/model/KorisnikDAO.java
+++ src/model/KorisnikDAO.java
@@ -2,10 +2,10 @@ package model;
 
 import misc.EntityManagerFactoryHolder;
 import javax.persistence.*;
+import java.util.List;
 
 public class KorisnikDAO {
 	private EntityManagerFactory emf = EntityManagerFactoryHolder.getEntityManagerFactory();
-	
 	public Korisnik searchByUserName(String name) {
 		try {
 			EntityManager em = emf.createEntityManager();
@@ -19,12 +19,18 @@ public class KorisnikDAO {
 			return null; //Makes this a total function, i.e. returns valid value for all inputs (I HOPE)
 		}
 	}
-	
 	public void addKorisnik(Korisnik kor) {
 		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
 		em.persist(kor);
 		em.getTransaction().commit();
 		em.close();
+	}
+	public List<Korisnik> getLimitedPending(int start, int ammount) {
+		EntityManager em = emf.createEntityManager();
+		List<Korisnik> ret = em.createQuery("SELECT a FROM Korisnik LIMIT " + String.valueOf(start) + ", " +
+				String.valueOf(ammount), Korisnik.class).getResultList();
+		em.close();
+		return ret;
 	}
 }
