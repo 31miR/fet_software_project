8c24e95b9dded8104ae23009a47eb533fd06824d
31miR <76799355+31miR@users.noreply.github.com.>
2024-08-27 20:47:04 +0200

    implement a working main function

--- src/Main.java
+++ src/Main.java
@@ -1,151 +1,19 @@
-
-
-
-import java.awt.Color;
-import javax.swing.border.BevelBorder;
 import java.awt.EventQueue;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.awt.Image;
-import javax.swing.ImageIcon;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
-
-
-import model.Administrator;
-import model.Korisnik;
-import model.Organizator;
-import model.AdministratorDAO;
-import model.OrganizatorDAO;
-import model.KorisnikDAO;
-
-import view.Register;
 import view.Login;
 
-
-    /**
-     * Create the frame.
-     */
-    public static void main () {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(450, 190, 1014, 597);
-        setResizable(false);
-        contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(contentPane);
-        contentPane.setLayout(null);
-        contentPane.setBackground(new Color(29, 190, 166)); 
-        setTitle("Karta");
-        ImageIcon icon = new ImageIcon("resources/logo.png");
-        setIconImage(icon.getImage());
-       
-        
-        
-        ImageIcon imageIcon = new ImageIcon("resources/logo.png"); // Postavi putanju do svoje slike
-        Image image = imageIcon.getImage(); // Transformacija slike
-        Image scaledImage = image.getScaledInstance(300, 300, java.awt.Image.SCALE_SMOOTH); // Promijeni veličinu slike
-        imageIcon = new ImageIcon(scaledImage); // Ponovo kreiraj ImageIcon sa promijenjenom slikom
-        
-        JLabel label = new JLabel(imageIcon);
-        label.setBounds(50, 100, 400, 300);
-        contentPane.add(label);
-        
-        contentPane.setBorder(new LineBorder(new Color(95,95,95), 7));
-        
-
-        JLabel lblNewLabel = new JLabel("Dobrodosli u Kartu!");
-        lblNewLabel.setForeground(Color.BLACK);
-        lblNewLabel.setFont(new Font("Chilanka", Font.ITALIC, 46));
-        lblNewLabel.setBounds(500, 12, 600, 95);
-        lblNewLabel.setForeground(new Color(95, 95, 95)); 
-        contentPane.add(lblNewLabel);
-
-        textField = new JTextField();
-        textField.setFont(new Font("Chilanka", Font.PLAIN, 32));
-        textField.setBounds(650, 170, 281, 50);
-        contentPane.add(textField);
-        textField.setColumns(10);
-
-        passwordField = new JPasswordField();
-        passwordField.setFont(new Font("Chilanka", Font.PLAIN, 32));
-        passwordField.setBounds(650, 286, 281, 50);
-        contentPane.add(passwordField);
-
-        JLabel lblUsername = new JLabel("Username");
-        lblUsername.setBackground(Color.BLACK);
-        lblUsername.setForeground(new Color(51, 51, 51)); 
-        lblUsername.setFont(new Font("Chilanka", Font.PLAIN, 31));
-        lblUsername.setBounds(450, 166, 193, 52);
-        contentPane.add(lblUsername);
-
-        JLabel lblPassword = new JLabel("Password");
-        lblPassword.setForeground(new Color(51, 51, 51)); 
-        lblPassword.setBackground(Color.CYAN);
-        lblPassword.setFont(new Font("Chilanka", Font.PLAIN, 31));
-        lblPassword.setBounds(450, 286, 193, 52);
-        contentPane.add(lblPassword);
-
-        loginButton = new JButton("Login");
-        loginButton.setFont(new Font("Chilanka", Font.PLAIN, 26));
-        loginButton.setBounds(545, 392, 162, 50);
-        loginButton.setForeground(new Color(51, 51, 51)); 
-        loginButton.setBorder(new BevelBorder(BevelBorder.RAISED));
-        loginButton.addActionListener(new ActionListener() {
-            	 @Override
-                 public void actionPerformed(ActionEvent e) {
-                     Login LoginView = new Login();
-                     LoginView.setVisible(true);
-                     dispose();
-                 }
-             });
-             
-
-        contentPane.add(loginButton);
-
-        label = new JLabel("");
-        label.setBounds(0, 0, 1008, 562);
-        contentPane.add(label);
-    
-    
-    RegisterButton= new JButton("Register");
-    RegisterButton.setFont(new Font("Chilanka", Font.PLAIN, 26));
-    RegisterButton.setBounds(800, 392, 162, 50);
-    RegisterButton.setForeground(new Color(51, 51, 51)); 
-    
-    RegisterButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            Register registerView = new Register();
-            registerView.setVisible(true);
-            dispose();
-        }
-    });
-    
-    contentPane.add(RegisterButton);
-    
-    
-    guestButton= new JButton("Login as guest");
-    guestButton.setFont(new Font("Chilanka", Font.PLAIN, 26));
-    guestButton.setBounds(600, 450, 300, 50);
-   
-    guestButton.setForeground(new Color(51, 51, 51)); 
-    
-    
-    contentPane.add(guestButton);
-    
-   
-}}
+public class Main {
+	public void main() {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
