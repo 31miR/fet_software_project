48e1654975923a305a9e2994863a005f7a8044e2
31miR <76799355+31miR@users.noreply.github.com>
2024-09-10 05:14:00 +0200

    add scripts that push in more pending profiles

---
+++ src/misc/addALotOfPendingProfiles.sql
@@ -0,0 +1,320 @@
+INSERT INTO Organizator VALUES ('organizator1',
+	'vimjezakon123',
+    'amer@linux.com',
+    'Amer',
+    'Hasanovic',
+    'nesto nesto, Tuzla',
+    '123456789',
+    true
+);
+INSERT INTO Organizator VALUES ('organizator2',
+	'vimjezakon123',
+    'amer@linux.com',
+    'Amer',
+    'Hasanovic',
+    'nesto nesto, Tuzla',
+    '123456789',
+    true
+);
+INSERT INTO Organizator VALUES ('organizator3',
+	'vimjezakon123',
+    'amer@linux.com',
+    'Amer',
+    'Hasanovic',
+    'nesto nesto, Tuzla',
+    '123456789',
+    true
+);
+INSERT INTO Organizator VALUES ('organizator4',
+	'vimjezakon123',
+    'amer@linux.com',
+    'Amer',
+    'Hasanovic',
+    'nesto nesto, Tuzla',
+    '123456789',
+    true
+);
+INSERT INTO Organizator VALUES ('organizator5',
+	'vimjezakon123',
+    'amer@linux.com',
+    'Amer',
+    'Hasanovic',
+    'nesto nesto, Tuzla',
+    '123456789',
+    true
+);
+INSERT INTO Organizator VALUES ('organizator6',
+	'vimjezakon123',
+    'amer@linux.com',
+    'Amer',
+    'Hasanovic',
+    'nesto nesto, Tuzla',
+    '123456789',
+    true
+);
+INSERT INTO Organizator VALUES ('organizator7',
+	'vimjezakon123',
+    'amer@linux.com',
+    'Amer',
+    'Hasanovic',
+    'nesto nesto, Tuzla',
+    '123456789',
+    true
+);
+INSERT INTO Organizator VALUES ('organizator8',
+	'vimjezakon123',
+    'amer@linux.com',
+    'Amer',
+    'Hasanovic',
+    'nesto nesto, Tuzla',
+    '123456789',
+    true
+);
+INSERT INTO Organizator VALUES ('organizator9',
+	'vimjezakon123',
+    'amer@linux.com',
+    'Amer',
+    'Hasanovic',
+    'nesto nesto, Tuzla',
+    '123456789',
+    true
+);
+INSERT INTO Korisnik VALUES ('korisnik1',
+	'teslaGoZoom',
+    'realelon123@gmail.com',
+    'Elon',
+    'Musk',
+    'Vida II, Gradacac',
+    '123456788',
+    1000000000,
+    0,
+    true
+);
+INSERT INTO Korisnik VALUES ('korisnik2',
+	'teslaGoZoom',
+    'realelon123@gmail.com',
+    'Elon',
+    'Musk',
+    'Vida II, Gradacac',
+    '123456788',
+    1000000000,
+    0,
+    true
+);
+INSERT INTO Korisnik VALUES ('korisnik3',
+	'teslaGoZoom',
+    'realelon123@gmail.com',
+    'Elon',
+    'Musk',
+    'Vida II, Gradacac',
+    '123456788',
+    1000000000,
+    0,
+    true
+);
+INSERT INTO Korisnik VALUES ('korisnik4',
+	'teslaGoZoom',
+    'realelon123@gmail.com',
+    'Elon',
+    'Musk',
+    'Vida II, Gradacac',
+    '123456788',
+    1000000000,
+    0,
+    true
+);
+INSERT INTO Korisnik VALUES ('korisnik5',
+	'teslaGoZoom',
+    'realelon123@gmail.com',
+    'Elon',
+    'Musk',
+    'Vida II, Gradacac',
+    '123456788',
+    1000000000,
+    0,
+    true
+);
+INSERT INTO Korisnik VALUES ('korisnik6',
+	'teslaGoZoom',
+    'realelon123@gmail.com',
+    'Elon',
+    'Musk',
+    'Vida II, Gradacac',
+    '123456788',
+    1000000000,
+    0,
+    true
+);
+INSERT INTO Korisnik VALUES ('korisnik7',
+	'teslaGoZoom',
+    'realelon123@gmail.com',
+    'Elon',
+    'Musk',
+    'Vida II, Gradacac',
+    '123456788',
+    1000000000,
+    0,
+    true
+);
+INSERT INTO Korisnik VALUES ('korisnik8',
+	'teslaGoZoom',
+    'realelon123@gmail.com',
+    'Elon',
+    'Musk',
+    'Vida II, Gradacac',
+    '123456788',
+    1000000000,
+    0,
+    true
+);
+INSERT INTO Korisnik VALUES ('korisnik9',
+	'teslaGoZoom',
+    'realelon123@gmail.com',
+    'Elon',
+    'Musk',
+    'Vida II, Gradacac',
+    '123456788',
+    1000000000,
+    0,
+    true
+);
+INSERT INTO Korisnik VALUES ('korisnik10',
+	'teslaGoZoom',
+    'realelon123@gmail.com',
+    'Elon',
+    'Musk',
+    'Vida II, Gradacac',
+    '123456788',
+    1000000000,
+    0,
+    true
+);
+INSERT INTO Organizator VALUES ('organizator10',
+	'vimjezakon123',
+    'amer@linux.com',
+    'Amer',
+    'Hasanovic',
+    'nesto nesto, Tuzla',
+    '123456789',
+    true
+);
+INSERT INTO Organizator VALUES ('organizator11',
+	'vimjezakon123',
+    'amer@linux.com',
+    'Amer',
+    'Hasanovic',
+    'nesto nesto, Tuzla',
+    '123456789',
+    true
+);
+INSERT INTO Organizator VALUES ('organizator12',
+	'vimjezakon123',
+    'amer@linux.com',
+    'Amer',
+    'Hasanovic',
+    'nesto nesto, Tuzla',
+    '123456789',
+    true
+);
+INSERT INTO Organizator VALUES ('organizator13',
+	'vimjezakon123',
+    'amer@linux.com',
+    'Amer',
+    'Hasanovic',
+    'nesto nesto, Tuzla',
+    '123456789',
+    true
+);
+INSERT INTO Organizator VALUES ('organizator14',
+	'vimjezakon123',
+    'amer@linux.com',
+    'Amer',
+    'Hasanovic',
+    'nesto nesto, Tuzla',
+    '123456789',
+    true
+);
+INSERT INTO Organizator VALUES ('organizator15',
+	'vimjezakon123',
+    'amer@linux.com',
+    'Amer',
+    'Hasanovic',
+    'nesto nesto, Tuzla',
+    '123456789',
+    true
+);
+INSERT INTO Organizator VALUES ('organizator16',
+	'vimjezakon123',
+    'amer@linux.com',
+    'Amer',
+    'Hasanovic',
+    'nesto nesto, Tuzla',
+    '123456789',
+    true
+);
+INSERT INTO Korisnik VALUES ('korisnik11',
+	'teslaGoZoom',
+    'realelon123@gmail.com',
+    'Elon',
+    'Musk',
+    'Vida II, Gradacac',
+    '123456788',
+    1000000000,
+    0,
+    true
+);
+INSERT INTO Korisnik VALUES ('korisnik12',
+	'teslaGoZoom',
+    'realelon123@gmail.com',
+    'Elon',
+    'Musk',
+    'Vida II, Gradacac',
+    '123456788',
+    1000000000,
+    0,
+    true
+);
+INSERT INTO Korisnik VALUES ('korisnik13',
+	'teslaGoZoom',
+    'realelon123@gmail.com',
+    'Elon',
+    'Musk',
+    'Vida II, Gradacac',
+    '123456788',
+    1000000000,
+    0,
+    true
+);
+INSERT INTO Korisnik VALUES ('korisnik14',
+	'teslaGoZoom',
+    'realelon123@gmail.com',
+    'Elon',
+    'Musk',
+    'Vida II, Gradacac',
+    '123456788',
+    1000000000,
+    0,
+    true
+);
+INSERT INTO Korisnik VALUES ('korisnik15',
+	'teslaGoZoom',
+    'realelon123@gmail.com',
+    'Elon',
+    'Musk',
+    'Vida II, Gradacac',
+    '123456788',
+    1000000000,
+    0,
+    true
+);
+INSERT INTO Korisnik VALUES ('korisnik16',
+	'teslaGoZoom',
+    'realelon123@gmail.com',
+    'Elon',
+    'Musk',
+    'Vida II, Gradacac',
+    '123456788',
+    1000000000,
+    0,
+    true
+);
